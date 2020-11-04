c940a6abdd7101ac9c5e4fd395803cf87618070b
unknown <intrstelr.007@gmail.com>
2020-11-04 17:26:43 +0530

    [Anuj] Added Hotel details

---
+++ Hotel_Reservation_System/HotelDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel_Reservation_System
+{
+    public class HotelDetail
+    {
+        public string hotel_Name { get; set; }
+        public int hotel_WeekdayRate { get; set; }
+        public int hotel_WeekendRate { get; set; }
+
+
+    }
+}
--- Hotel_Reservation_System/Program.cs
+++ Hotel_Reservation_System/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Hotel_Reservation_System
 {
@@ -7,6 +8,18 @@ namespace Hotel_Reservation_System
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hotel reservation Program");
+
+            List<HotelDetail> hotel_list = new List<HotelDetail> {
+                new HotelDetail { hotel_Name="Lakewood",hotel_WeekdayRate=110,hotel_WeekendRate=80},
+                new HotelDetail{ hotel_Name="Bridgewood",hotel_WeekdayRate=160,hotel_WeekendRate=110},
+                new HotelDetail{hotel_Name="Ridgewood",hotel_WeekdayRate=220,hotel_WeekendRate=100}
+
+            };
+
+            foreach(var hotel in hotel_list)
+            {
+                Console.WriteLine(hotel.hotel_Name);
+            }
         }
     }
 }
